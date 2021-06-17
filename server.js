ea5d2a119539a6b64486a75c749fbebfd166b03f
Cosmin <cosmin.aftanase@yahoo.com>
2021-06-17 09:57:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = require("./routes")
 const path = require("path")
 const mongoose = require("mongoose")
 const axios = require("axios")
-const { middleWare } = require("./middleWare")
+const { middleWare } = require("./middleware")
 const port = process.env.PORT || 8000
 require("dotenv").config()
 
