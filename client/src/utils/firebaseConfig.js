a04011e979162e6ea1a58a12cd4fa91e0f84b5d4
Cosmin <cosmin.aftanase@yahoo.com>
2021-06-24 13:59:18 +0300

    Update firebaseConfig.js

--- client/src/utils/firebaseConfig.js
+++ client/src/utils/firebaseConfig.js
@@ -1,11 +1,12 @@
 const config = {
-  apiKey: "AIzaSyDijdSuRqeZBt7_N000pjVNPKgsvKNd-MY",
-  authDomain: "pocketide-cbf82.firebaseapp.com",
-  projectId: "pocketide-cbf82",
-  storageBucket: "pocketide-cbf82.appspot.com",
-  messagingSenderId: "232459301858",
-  appId: "1:232459301858:web:09b167907e20c9f39cffbb",
   measurementId: "G-62H2TXM9HZ"
-};//todo
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_SID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID,
+}
 
 export default config
